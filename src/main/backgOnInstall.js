59c5437f09f44f9b8b456e7d504a90e451203924
Jornexe <62523659+Jornexe@users.noreply.github.com>
2022-04-26 16:37:35 +0200

    Large Rework Y R
    Reworked message copying to be loss free.
    Reworked insertion to be interval based instead of event based.
    Moved leaderboard to the top.
    Filter addition: broadcaster.
    npm installed jquery.
    Added ability to hide filtered chat window.

--- src/main/backgOnInstall.js
+++ src/main/backgOnInstall.js
@@ -1,23 +1,30 @@
-let vip = true;
-let mod = true;
-let verified = false;
-let prime = false;
-let antiSpam = true;
-let subbed = false;
+// works ↓
+let enabled = true; // enable program
+let broadcaster = true; // is the user a broadcaster
+let vip = true; // is the user a VIP
+let mod = true; // is the user a Moderator
+let verified = false; // is the user Verified
+let prime = false; // is the user Prime
+let antiSpam = true; // ← this one is always on atm. Prevents duplicate messages within the same timeframe
+let subbed = false; // does not work ↓
 let minsub = 1;
 let minSubGift = false;
 let minSubGiftAmount = 1;
-let minCheerMsg = false;
-let minCheerMsgAmount = 1;
+let minCheerMsg = false; // working on this
+let minCheerMsgAmount = 1; // working on this
 let minCheerBadge = false;
 let minCheerBadgeAmount = 1;
 
 
 chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ enabled });
+    chrome.storage.sync.set({ broadcaster });
     chrome.storage.sync.set({ vip });
     chrome.storage.sync.set({ mod });
     chrome.storage.sync.set({ verified });
+    chrome.storage.sync.set({ prime });
     chrome.storage.sync.set({ antiSpam });
+
     chrome.storage.sync.set({ subbed });
     chrome.storage.sync.set({ minsub });
     chrome.storage.sync.set({ minSubGift });
--- src/main/scriChat.js
+++ src/main/scriChat.js
@@ -1,73 +1,111 @@
-console.log("YEEEEEEEEEEEET", document.getElementById("altChatDiv"));
-document.addEventListener("DOMContentLoaded", loaded(), false);
-window.addEventListener("popstate", function () {
-    console.log("popstate called! ==========================================");
-}, false);
-window.addEventListener("locationchange", function () {
-    console.log("locationchange called! ==========================================");
-}, false);
+// import jquery from 'jquery';
+
+// console.log("YEEEEEEEEEEEET", document.getElementById("altChatDiv"));
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     document.head.prepend("<link rel=\"stylesheet\" href=\"./assets/exstyle.css\">");
+// }, false);
+
+setInterval(function () { if (document.getElementById("altChatDiv") === null) { loaded() } }, 5000);
+
 // fix the event so it calls when switching to another channel and not just on reload or first page load.
 
 // adds easier identification and creates dupe target.
 function loaded() {
     console.log("LOADED CALLLED ----> ", document.readyState);
-    if (document.readyState === "interactive") {
-        if (document.getElementById("altChatDiv") === null) {
-            console.log("YEEEEEEEEEEEEEE IT WOOOORSSSSSSSSSSSKKKK");
+    if (document.getElementById("altChatDiv") === null) {
+        
 
-            // sets up id for copying chat to filter
-            document.querySelectorAll('[role="log"]').forEach(function (x) {
-                x.setAttribute("id", "chatLogDefault");
-            });
+        // sets up id for copying chat to filter
+        document.querySelectorAll('[role="log"]').forEach(function (x) {
+            x.setAttribute("id", "chatLogDefault");
+        });
 
-            // creates filtered chat box
-            locate = document.getElementsByClassName("chat-room__content");
-            altChatDiv = document.createElement("div");
-            altChatDiv.setAttribute("id", "altChatDiv");
-            altChatDiv.setAttribute(
-                "style",
-                "height: 30%; position: relative; overflow-y: auto; overflow-x: hidden;"
-            );
-            Array.prototype.forEach.call(locate, function (x) {
-                x.prepend(altChatDiv);
-            });
+        // creates filtered chat box
+        locate = document.getElementsByClassName("chat-room__content");
 
-            // welcome message
-            welcome1 = document.createElement("span");
-            welcome1.setAttribute("class", "CoreText-sc-cpl358-0 Layout-sc-nxg1ff-0 chat-line__status");
+        // supposed to make the scroll bar fancy.
+        // does nothing atm though.
+        scrollbar = document.createElement("div");
+        scrollbar.setAttribute("id", "scrollbar-style-1");
+        scrollbar.setAttribute(
+            "style",
+            "height: 30%; position: relative; overflow-y: auto; overflow-x: hidden;"
+        );
+
+        altChatDiv = document.createElement("div");
+        altChatDiv.setAttribute("id", "altChatDiv");
+        altChatDiv.setAttribute("class", "scrollbar-style-1");
+        altChatDiv.setAttribute(
+            "style",
+            "height: 30%; position: relative; overflow-y: auto; overflow-x: hidden; border-bottom: solid; border-color: gray;"
+        );
+
+        Array.prototype.forEach.call(locate, function (x) {
+            x.prepend(altChatDiv);
+        });
+
+        // makes the alt chat visible and forces main chat to only take so much of the screen.
+        // ADD: user defined height
+        Array.prototype.forEach.call(
+            document.getElementsByClassName("chat-list--default"),
+            function (x) {
+                x.setAttribute("id", "blockhidden");
+                x.setAttribute("style", "height: 40% !important");
+            }
+        );
+
+        // welcome message
+        welcome1 = document.createElement("span");
+        welcome1.setAttribute("class", "CoreText-sc-cpl358-0 Layout-sc-nxg1ff-0 chat-line__status");
+        console.log("------->", document.getElementsByClassName("seventv-message-context"));
+        if (false) {
+            welcome1.innerHTML = "Please install 7TV chrome extension for this to work";
+        }
+        else {
             welcome1.innerHTML = "Welcome to the filter!";
-            altChatDiv.appendChild(welcome1);
-
-            // makes the alt chat visible and forces main chat to only take so much of the screen.
-            // ADD: user defined height
-            Array.prototype.forEach.call(
-                document.getElementsByClassName("chat-list--default"),
-                function (x) {
-                    x.setAttribute("style", "height: 40% !important");
-                }
-            );
-
             console.log("YEEEEEEEEEEEET", document.getElementById("altChatDiv"));
             setupListener();
         }
+        altChatDiv.appendChild(welcome1);
+
+        // makes the leaderboard not be inbetween the chatboxes
+        let swapPos = setInterval(function () {
+            // console.log("next sibling to altChat", document.getElementById("altChatDiv").nextElementSibling.children[0].children[0].classList.contains("channel-leaderboard"));
+            leaderboard = document.getElementById("altChatDiv").nextElementSibling;
+            // console.log("swdfåpoakswedfo",leaderboard.children);
+            if (leaderboard.children.length != 0 && leaderboard.children[0].children[0].classList.contains("channel-leaderboard")) {
+                console.log("Swapped Leaderboard Position");
+                document.getElementById("altChatDiv").parentNode.insertBefore(leaderboard, document.getElementById("altChatDiv"));
+                clearInterval(swapPos)
+            }
+        }, 5000);
     }
 }
 
 // add variable here after inserting it in options.html
 // var must have same name as id
 // temp variables
+
+// shows/hides the extension
 enabled = true;
+// checks for badges
+broadcaster = true;
 vip = true;
 mod = true;
 verified = true;
 prime = true;
+// currently always true - prevents duplicate messages within the same timeframe.
 antiSpam = true;
+// not implemented
 subbed = true;
 minsub = 999999999;
 minSubGift = true;
 minSubGiftAmount = 999999999;
+// working on - 
 minCheerMsg = true;
 minCheerMsgAmount = 999999999;
+// not implemented
 minCheerBadge = true;
 minCheerBadgeAmount = 999999999;
 
@@ -79,13 +117,34 @@ chrome.storage.sync.get(null, function (itemsOBJ) {
             window[storKey] = itemsOBJ[storKey];
         }
     }
+    altChatVisibility(enabled);
 });
 
 // updates local variables when they are changed
 chrome.storage.onChanged.addListener(function (storag) {
+    console.log("=============????????", storag);
     window[Object.keys(storag)[0]] = Object.values(Object.values(storag)[0])[0];
     console.log(Object.keys(storag)[0], "updated to", Object.values(Object.values(storag)[0])[0]);
+    if (Object.keys(storag)[0] == "enabled") {
+        altChatVisibility(Object.values(Object.values(storag)[0])[0]);
+    }
 });
+
+function altChatVisibility(bool) {
+    if (document.getElementById("altChatDiv") != "undefined" && document.getElementById("altChatDiv") != null) {
+        console.log("works")
+        if (bool) {
+            //document.getElementById("blockhidden").style.height = "40% !important";
+            document.getElementById("blockhidden").setAttribute("style", "height: 40% !important");
+            document.getElementById("altChatDiv").setAttribute("style", "height: 30%; position: relative; overflow-y: auto; overflow-x: hidden;");
+        }
+        else {
+            // document.getElementById("blockhidden").style.height = "";
+            document.getElementById("blockhidden").setAttribute("style", "height: 100% !important");
+            document.getElementById("altChatDiv").setAttribute("style", "height: 0%; position: relative; overflow-y: auto; overflow-x: hidden;");
+        }
+    }
+}
 
 // REWORKED NODE INSERTER
 function setupListener() {
@@ -93,18 +152,24 @@ function setupListener() {
     altChatDivId = document.getElementById("altChatDiv");
     chatLogDefault = document.getElementById("chatLogDefault");
     // checks for new messages in chat
-    chatLogDefault.addEventListener("DOMNodeInserted", function () {
+    chatLogDefault.addEventListener("DOMNodeInserted", function (insertedNode) {
         // Gets all information about message
-
-        if (chatLogDefault.lastChild.lastChild != "undefined" && chatLogDefault.lastChild.lastChild != null) {
-            var child = chatLogDefault.lastChild.lastChild;
+        // console.log("--->", insertedNode, insertedNode.target.classList);
+        // WHAT THE ACTUAL FUCK WAS I THINKING
+        // imma need to rework whatever i did here.
+        // if (insertedNode.target.classList[0] == "chat-line__message") {
+        if (insertedNode.target.classList && insertedNode.target.classList.contains("chat-line__message")) {
+            // console.log("Inserted Node Target", insertedNode.target);
+            var child = insertedNode.target;
             // ADD: user defined chat filter <--------------
-            if (vip || mod || verified || prime) {
+            if (vip || mod || verified || prime || broadcaster) {
                 checkBadge = [];
-                for (let i = 0, arr = child.getElementsByClassName("chat-badge"); i < arr.length; i++) {
-                    // console.log(arr[i]);
-                    arrTib = arr[i].getAttribute("alt");
+                for (let badges of child.getElementsByClassName("chat-badge")) {
+                    arrTib = badges.getAttribute("alt");
                     if (arrTib == "VIP" && vip) {
+                        checkBadge.push(true);
+                    }
+                    else if (arrTib == "Broadcaster" && broadcaster) {
                         checkBadge.push(true);
                     }
                     else if (arrTib == "Moderator" && mod) {
@@ -118,7 +183,8 @@ function setupListener() {
                     }
                     else checkBadge.push(false);
                 }
-                if (checkBadge.includes(true)) { } else return;
+                if (checkBadge.includes(true) || minCheerMsg) { } else return;
+
                 // // work out a method to filter these:
                 // subbed = true;
                 // minsub = 999999999;
@@ -131,48 +197,67 @@ function setupListener() {
             }
             // ADD: user defined chat filter <--------------
 
-            // check if object is from a client. 
-            if (child.getElementsByClassName("chat-author__display-name")[0] != "undefined" && child.getElementsByClassName("chat-author__display-name")[0] != null) {
-                var childName = child.getElementsByClassName("chat-author__display-name")[0].textContent;
-                // class: seventv-message-context
-                // gets all message content
-                var childContent = "";
-                if (child.getElementsByClassName("seventv-message-context") != "undefined" && child.getElementsByClassName("seventv-message-context") != null) {
-                    for (let y of child.getElementsByClassName("seventv-message-context")) {
-                        for (let x of y.children) {
-                            if (x.classList.contains("seventv-emote")) {
-                                childContent += x.children[0].children[0].getAttribute("alt");
-                            }
-                            else if (x.children[0] && x.children[0].nodeName == "IMG") {
-                                //console.log("IMG", x)
-                                childContent += x.children[0].getAttribute("alt");
-                            }
-                            else if (x.textContent) {
-                                childContent += x.textContent;
-                            }
-                            else {
-                                childContent += "-EMPTY-";
-                            }
-                        }
+            // check if object is from a client.
+            // console.log("zzxc", child.getElementsByClassName("chat-author__display-name")[0], "--->", child.getElementsByClassName("chat-author__display-name")[0]);
+
+            var childName = child.getElementsByClassName("chat-author__display-name")[0].textContent;
+            // class: seventv-message-context
+            // gets all message content
+            var childContent = "";
+            // console.log("seven", child.getElementsByClassName("seventv-message-context"), child.getElementsByClassName("seventv-message-context")[0]);
+            console.log("ChildObj", child);
+            var messageContent;
+            if (child.children[0].children[1] != "undefined" && child.children[0].children[1] != null) {
+                messageContent = child.children[0].children[1].children[0].children[0].lastChild
+            }
+            else {
+                messageContent = child.children[0].children[0].children[0].children[0].lastChild
+            }
+            // console.log("msgCont",messageContent)
+            for (let messageBit of messageContent.children) {
+                if (messageBit.classList.contains("seventv-emote")) {
+                    if (minCheerMsg && messageBit.children[0].children[0].getAttribute("alt") == "Cheer") {
+
+                    }
+                    else {
+                        childContent += messageBit.children[0].children[0].getAttribute("alt");
                     }
                 }
-                else if (child.getElementsByClassName("chat-line__no-background") != "undefined" && child.getElementsByClassName("chat-line__no-background") != null){
-                    for (let y of child.getElementsByClassName("chat-line__no-background")){
-                        for (let x of y.lastChild.children){
-                            // duplicate the obove seventv function to work without 7tv
-                        }
-                    }
+                else if (messageBit.children[0] && messageBit.children[0].nodeName == "IMG") {
+                    //console.log("IMG", x)
+                    childContent += messageBit.children[0].getAttribute("alt");
                 }
-                if (child.getElementsByClassName("chat-line__timestamp")[0].textContent != null) {
-                    var childTStamp = child.getElementsByClassName("chat-line__timestamp")[0].textContent;
+                else if (messageBit.textContent) {
+                    childContent += messageBit.textContent;
                 }
                 else {
-                    var childTStamp = "-NO-TIMESTAMP-";
+                    childContent += "-EMPTY-";
                 }
             }
+            childContent = childContent.replace(" ","");
+            // console.log("childContent -currently disabled to expected empty-",childContent);
+
+            // for some reason twitch decided to mix chat messages with usernames and everything else unlike what 7tv does to the messages.
+            // think imma just require 7tv to make all this easier.
+            // else if (child.getElementsByClassName("chat-line__no-background") != "undefined" && child.getElementsByClassName("chat-line__no-background") != null){
+            //     for (let y of child.getElementsByClassName("chat-line__no-background")){
+            //         for (let x of y.lastChild.children){
+            //             // duplicate the obove seventv function to work without 7tv
+            //         }
+            //     }
+            // }
+            if (child.getElementsByClassName("chat-line__timestamp")[0].textContent != null) {
+                var childTStamp = child.getElementsByClassName("chat-line__timestamp")[0].textContent;
+            }
+            else {
+                var childTStamp = "-NO-TIMESTAMP-";
+            }
+
         }
         else return;
         // Ensures that these variables actually exist
+        console.log(childName, childContent, childTStamp);
+        console.log(childContent);
         if (childName && childContent && childTStamp) {
             // COPIES FROM MAIN CHAT TO FILTERED CHAT WINDOW
             if (!document.getElementById(childName + childContent + childTStamp)) {
@@ -199,6 +284,11 @@ function updateScroll() {
     }
 }
 
-
-
+// checks if exists
+function doesExist(object) {
+    if (object && object != "undefined" && object != null) {
+        return true
+    }
+    else return false
+}
 
